e8cb7ff6e87d828d9529d1da99cd0f57967f2a78
Developer Kitchen <developerkitchentr@gmail.com>
2022-11-21 23:19:58 +0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -102,7 +102,7 @@ const Home: NextPage<Props> = (
                     <OurSupportersScopeLegendary sponsors={sponsors}/>
                     
                     <div className="get-code-outer">
-                        <h1>Bize Destek Olun</>
+                        <h2>Bize Destek Olun</h2>
                         <div className="sm:flex sm:flex-row items-center">
                             <div className="basis-1/1 sm:basis-1/2 px-3 mb-5">
                                 <Image src="/images/bitcoin-bottom.svg" alt="" width={96} height={38}/>
