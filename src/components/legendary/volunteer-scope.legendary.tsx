7fa40cca33dd84c113d9e6ae725cc2a6c3d2737c
Ahmet aytangil <ahmetaytangil@gmail.com>
2022-12-12 23:56:43 +0300

    feats

--- src/components/legendary/volunteer-scope.legendary.tsx
+++ src/components/legendary/volunteer-scope.legendary.tsx
@@ -9,7 +9,7 @@ interface Props {
 const VolunteerScopeLegendary = ({volunteers}: Props) => {
 
     return (
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 mb-14 mt-10">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-14 mt-10">
             {volunteers?.map(({attributes, id}) => (
                 <div className="volunteer-cell" key={id}>
                     <Image src={getImageSrc(getImageUrl(attributes.photo))} alt="" width={310} height={310} />
